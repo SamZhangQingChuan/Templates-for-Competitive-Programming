a9bde2b97479227e61065f92cc34c3df03712203
samzhang <samzhangqingchuan@icloud.com>
2020-04-18 14:40:28 +1200

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -1,7 +1,6 @@
 void calcNext(const string &s, int nxt[]) {
     int p = nxt[0] = -1;
-    REP(i, 1, sz(s))
-    {
+    REP(i, 1, sz(s)){
         while (p != -1 && s[p + 1] != s[i])p = nxt[p];
         p += s[p + 1] == s[i];
         nxt[i] = p;
@@ -9,17 +8,17 @@ void calcNext(const string &s, int nxt[]) {
 }
 
 
-/**
- * z[i] = lcp(s[i:], s[0:])
- *
- * time complexity: O(n)
- **/
-std::vector<int> ext_kmp(char *s, int n) {
-    std::vector<int> z(n, 0);
-    for (int i = 1, x = 0, y = 0; i < n; ++i) {
-        if (i <= y) z[i] = std::min(y - i, z[i - x]);
-        while (i + z[i] < n && s[i + z[i]] == s[z[i]]) ++z[i];
-        if (y <= i + z[i]) x = i, y = i + z[i];
+vector<int> exkmp(const string &s) {
+    int n = s.size();
+    vector<int> z(n);
+    int x = 0, y = 0;
+    for(int i = 1; i < n; i++) {
+        z[i] = max(0, min(z[i - x], y - i + 1));
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            x = i;
+            y = i + z[i];
+            z[i]++;
+        }
     }
     z[0] = n;
     return z;
