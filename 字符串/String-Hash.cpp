c3e6105ddc8855ba3f27570e670a8cc62d72794c
samzhang <samzhangqingchuan@icloud.com>
2020-05-13 02:11:55 +1200

    updated hashing

--- 字符串/String-Hash.cpp
+++ 字符串/String-Hash.cpp
@@ -1,18 +1,66 @@
-//1-based
+// 1-based
+// 支持char/int/ll
+// push  - 加在末尾
+// build - 重建
 struct Hashing {
-    ull seed, Mod;
-    vector <ull> hs, pw;
+    array<ll, 2> seed, mod;
+    array<vector<ll>, 2> hs, pw;
     
-    Hashing(ull seed = 31, ull Mod = 1000173169) : seed(seed), Mod(Mod) {}
-    
-    void build(int n, const char* s) {
-        hs.assign(n + 1, 0);
-        pw.assign(n + 1, 1);
-        pw[0] = 1;
-        for (int i = 1; i <= n; ++i) pw[i] = pw[i - 1] * seed % Mod;
-        for (int i = 1; i <= n; ++i) hs[i] = (hs[i - 1] * seed + s[i]) % Mod;
+    Hashing(array<ll, 2> seed = {28, 31},
+            array<ll, 2> mod = {1000000007, 1000173169}) : seed(seed), mod(mod) {
+        init();
     }
     
-    ull hash(int l, int r) { return (hs[r] - hs[l - 1] * pw[r - l + 1] % Mod + Mod) % Mod; }
+    void init() {
+        hs[0] = hs[1] = {0};
+        pw[0] = pw[1] = {1};
+    }
+    
+    void push(ll ch) {
+        assert(ch != 0);
+        for(int i = 0; i < 2; i++) {
+            pw[i].push_back(pw[i].back() * seed[i] % mod[i]);
+            hs[i].push_back((hs[i].back() * seed[i] + ch) % mod[i]);
+        }
+    }
+    
+    void pop() {
+        assert(size() > 1);
+        for(int i = 0; i < 2; i++) {
+            pw[i].pop_back();
+            hs[i].pop_back();
+        }
+    }
+    
+    
+    template<typename T>
+    void build(T begin, T end) {
+        init();
+        while(begin != end) {
+            push(*(begin++));
+        }
+    }
+    
+    template<typename T>
+    void push(T begin, T end) {
+        while(begin != end) {
+            push(*(begin++));
+        }
+    }
+    
+    array<ll, 2> hash(int l, int r) {
+        l--;
+        assert(l <= r and r <= size());
+        array<ll, 2> ans{0, 0};
+        if(l >= r)return ans;
+        for(int i = 0; i < 2; i++) {
+            ans[i] = (hs[i][r] - hs[i][l] * pw[i][r - l]) % mod[i];
+            if(ans[i] < 0)ans[i] += mod[i];
+        }
+        return ans;
+    }
+    
+    int size() {
+        return hs[0].size() - 1;
+    }
 };
-
