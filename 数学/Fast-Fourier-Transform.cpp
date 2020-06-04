4630bc2e274ffedee7ab186d5c8122d99fbf69ad
samzhang <samzhangqingchuan@icloud.com>
2020-06-04 23:03:04 +1200

    多项式

--- 数学/Fast-Fourier-Transform.cpp
+++ 数学/Fast-Fourier-Transform.cpp
@@ -1,64 +1,64 @@
 namespace fft {
-    const int N = 1 << 20, M = 31768;
+    const int N = 1<<20, M = 31768;
     
     struct Complex {
         double x, y;
         
-        Complex() { x = y = 0; }
+        Complex () { x = y = 0; }
         
-        Complex(double _x, double _y) { x = _x, y = _y; }
+        Complex (double _x, double _y) { x = _x, y = _y; }
         
-        Complex operator+(const Complex &r) const {
-            return Complex(x + r.x, y + r.y);
+        Complex operator+ (const Complex &r) const {
+            return Complex (x + r.x, y + r.y);
         }
         
-        Complex operator-(const Complex &r) const {
-            return Complex(x - r.x, y - r.y);
+        Complex operator- (const Complex &r) const {
+            return Complex (x - r.x, y - r.y);
         }
         
-        Complex operator*(const double k) const {
-            return Complex(x * k, y * k);
+        Complex operator* (const double k) const {
+            return Complex (x * k, y * k);
         }
         
-        Complex operator/(const double k) const {
-            return Complex(x / k, y / k);
+        Complex operator/ (const double k) const {
+            return Complex (x / k, y / k);
         }
         
-        Complex operator*(const Complex &r) const {
-            return Complex(x * r.x - y * r.y, x * r.y + y * r.x);
+        Complex operator* (const Complex &r) const {
+            return Complex (x * r.x - y * r.y, x * r.y + y * r.x);
         }
         
-        int operator=(const int a) {
-            *this = Complex(a, 0);
+        int operator= (const int a) {
+            *this = Complex (a, 0);
             return a;
         }
         
-        Complex conj() const {
-            return Complex(x, -y);
+        Complex conj () const {
+            return Complex (x, -y);
         }
     };
     
-    const double pi = acos(-1.0);
+    const double pi = acos (-1.0);
     Complex w[N];
     int rev[N];
     
-    void init(int L) {
-        int n = 1 << L;
+    void init (int L) {
+        int n = 1<<L;
         for (int i = 0; i < n; ++i) {
             double ang = 2 * pi * i / n;
-            w[i] = Complex(cos(ang), sin(ang));
-            rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (L - 1));
+            w[i] = Complex (cos (ang), sin (ang));
+            rev[i] = (rev[i>>1]>>1) | ((i & 1)<<(L - 1));
         }
     }
     
-    void trans(Complex P[], int n, int oper) {
+    void trans (Complex P[], int n, int oper) {
         for (int i = 0; i < n; i++) {
             if (i < rev[i]) {
-                std::swap(P[i], P[rev[i]]);
+                std::swap (P[i], P[rev[i]]);
             }
         }
-        for (int d = 0; (1 << d) < n; d++) {
-            int m = 1 << d, m2 = m * 2, rm = n / m2;
+        for (int d = 0; (1<<d) < n; d++) {
+            int m = 1<<d, m2 = m * 2, rm = n / m2;
             for (int i = 0; i < n; i += m2) {
                 for (int j = 0; j < m; j++) {
                     Complex &P1 = P[i + j + m], &P2 = P[i + j];
@@ -77,91 +77,89 @@ namespace fft {
     
     Complex A[N], B[N], C1[N], C2[N];
     
-    std::vector <ll> conv(const std::vector<int> &a, const std::vector<int> &b) {
-        int n = a.size(), m = b.size(), L = 0, s = 1;
+    std::vector<ll> conv (const std::vector<int> &a, const std::vector<int> &b) {
+        int n = a.size (), m = b.size (), L = 0, s = 1;
         while (s <= n + m - 2) s <<= 1, ++L;
-        init(L);
+        init (L);
         for (int i = 0; i < s; ++i) {
-            A[i] = i < n ? Complex(a[i], 0) : Complex();
-            B[i] = i < m ? Complex(b[i], 0) : Complex();
+            A[i] = i < n ? Complex (a[i], 0) : Complex ();
+            B[i] = i < m ? Complex (b[i], 0) : Complex ();
         }
-        trans(A, s, 1);
-        trans(B, s, 1);
+        trans (A, s, 1);
+        trans (B, s, 1);
         for (int i = 0; i < s; ++i) {
             A[i] = A[i] * B[i];
         }
         for (int i = 0; i < s; ++i) {
-            w[i] = w[i].conj();
+            w[i] = w[i].conj ();
         }
-        trans(A, s, -1);
-        std::vector <ll> res(n + m - 1);
+        trans (A, s, -1);
+        std::vector<ll> res (n + m - 1);
         for (int i = 0; i < n + m - 1; ++i) {
-            res[i] = (ll)(A[i].x + 0.5);
+            res[i] = (ll) (A[i].x + 0.5);
         }
         return res;
     }
     
-    std::vector <ll> fast_conv(const std::vector<int> &a, const std::vector<int> &b) {
-        int n = a.size(), m = b.size(), L = 0, s = 1;
+    std::vector<ll> fast_conv (const std::vector<int> &a, const std::vector<int> &b) {
+        int n = a.size (), m = b.size (), L = 0, s = 1;
         for (; s <= n + m - 2; s <<= 1, ++L);
         s >>= 1, --L;
-        init(L);
+        init (L);
         for (int i = 0; i < s; ++i) {
-            A[i].x = (i << 1) < n ? a[i << 1] : 0;
-            B[i].x = (i << 1) < m ? b[i << 1] : 0;
-            A[i].y = (i << 1 | 1) < n ? a[i << 1 | 1] : 0;
-            B[i].y = (i << 1 | 1) < m ? b[i << 1 | 1] : 0;
+            A[i].x = (i<<1) < n ? a[i<<1] : 0;
+            B[i].x = (i<<1) < m ? b[i<<1] : 0;
+            A[i].y = (i<<1 | 1) < n ? a[i<<1 | 1] : 0;
+            B[i].y = (i<<1 | 1) < m ? b[i<<1 | 1] : 0;
         }
-        trans(A, s, 1);
-        trans(B, s, 1);
+        trans (A, s, 1);
+        trans (B, s, 1);
         for (int i = 0; i < s; ++i) {
             int j = (s - i) & (s - 1);
-            C1[i] = (Complex(4, 0) * (A[j] * B[j]).conj() -
-                     (A[j].conj() - A[i]) * (B[j].conj() - B[i]) * (w[i] + Complex(1, 0))) * Complex(0, 0.25);
+            C1[i] = (Complex (4, 0) * (A[j] * B[j]).conj () -
+                     (A[j].conj () - A[i]) * (B[j].conj () - B[i]) * (w[i] + Complex (1, 0))) * Complex (0, 0.25);
         }
-        std::reverse(w + 1, w + s);
-        trans(C1, s, -1);
-        std::vector <ll> res(n + m);
+        std::reverse (w + 1, w + s);
+        trans (C1, s, -1);
+        std::vector<ll> res (n + m);
         for (int i = 0; i <= (n + m - 1) / 2; ++i) {
-            res[i << 1] = ll(C1[i].y + 0.5);
-            res[i << 1 | 1] = ll(C1[i].x + 0.5);
+            res[i<<1] = ll (C1[i].y + 0.5);
+            res[i<<1 | 1] = ll (C1[i].x + 0.5);
         }
-        res.resize(n + m - 1);
+        res.resize (n + m - 1);
         return res;
     }
     
     // arbitrary modulo convolution
-    // n,m = degree + 1
-    // x^2 + 2x +1 => n = 3
-    void conv(int a[], int b[], int n, int m, int mod, int res[]) {
+    void conv (int a[], int b[], int n, int m, int mod, int res[]) {
         int s = 1, L = 0;
         while (s <= n + m - 2) s <<= 1, ++L;
-        init(L);
+        init (L);
         for (int i = 0; i < s; ++i) {
-            A[i] = i < n ? Complex(a[i] / M, a[i] % M) : Complex();
-            B[i] = i < m ? Complex(b[i] / M, b[i] % M) : Complex();
+            A[i] = i < n ? Complex (a[i] / M, a[i] % M) : Complex ();
+            B[i] = i < m ? Complex (b[i] / M, b[i] % M) : Complex ();
         }
-        trans(A, s, 1);
-        trans(B, s, 1);
+        trans (A, s, 1);
+        trans (B, s, 1);
         for (int i = 0; i < s; ++i) {
             int j = i ? s - i : i;
-            Complex a1 = (A[i] + A[j].conj()) * Complex(0.5, 0);
-            Complex a2 = (A[i] - A[j].conj()) * Complex(0, -0.5);
-            Complex b1 = (B[i] + B[j].conj()) * Complex(0.5, 0);
-            Complex b2 = (B[i] - B[j].conj()) * Complex(0, -0.5);
+            Complex a1 = (A[i] + A[j].conj ()) * Complex (0.5, 0);
+            Complex a2 = (A[i] - A[j].conj ()) * Complex (0, -0.5);
+            Complex b1 = (B[i] + B[j].conj ()) * Complex (0.5, 0);
+            Complex b2 = (B[i] - B[j].conj ()) * Complex (0, -0.5);
             Complex c11 = a1 * b1, c12 = a1 * b2;
             Complex c21 = a2 * b1, c22 = a2 * b2;
-            C1[j] = c11 + c12 * Complex(0, 1);
-            C2[j] = c21 + c22 * Complex(0, 1);
+            C1[j] = c11 + c12 * Complex (0, 1);
+            C2[j] = c21 + c22 * Complex (0, 1);
         }
-        trans(C1, s, -1);
-        trans(C2, s, -1);
+        trans (C1, s, -1);
+        trans (C2, s, -1);
         for (int i = 0; i < n + m - 1; ++i) {
-            int x = ll(C1[i].x + 0.5) % mod;
-            int y1 = ll(C1[i].y + 0.5) % mod;
-            int y2 = ll(C2[i].x + 0.5) % mod;
-            int z = ll(C2[i].y + 0.5) % mod;
-            res[i] = ((ll) x * M * M + (ll)(y1 + y2) * M + z) % mod;
+            int x = ll (C1[i].x + 0.5) % mod;
+            int y1 = ll (C1[i].y + 0.5) % mod;
+            int y2 = ll (C2[i].x + 0.5) % mod;
+            int z = ll (C2[i].y + 0.5) % mod;
+            res[i] = ((ll) x * M * M + (ll) (y1 + y2) * M + z) % mod;
         }
     }
 }
---
+++ 数学/Polynomial.cpp
@@ -0,0 +1,147 @@
+using fft::N;
+
+namespace Poly {
+    int tmp1[N], tmp2[N], tmp3[N], mod = ;
+    int iv[N];
+    
+    void init () {
+        iv[1] = 1;
+        for (int i = 2; i < N; ++i) {
+            iv[i] = (mod - (ll) mod / i * iv[mod % i] % mod);
+        }
+    }
+    
+    
+    // res = 1 / poly
+    void inv (int poly[], int res[], int n) {
+        int deg = n - 1;
+        std::vector<int> degs;
+        while (deg > 0) {
+            degs.push_back (deg);
+            deg >>= 1;
+        }
+        std::reverse (degs.begin (), degs.end ());
+        res[0] = inverse (poly[0], mod);
+        deg = 1;
+        for (int t: degs) {
+            fft::conv (poly, res, t + 1, deg, mod, tmp1);
+            fft::conv (res, tmp1 + deg, t + 1 - deg, t + 1 - deg, mod, tmp1);
+            for (int i = 0; i < t + 1 - deg; ++i) {
+                res[i + deg] = mod - tmp1[i];
+            }
+            deg = t + 1;
+        }
+    }
+    
+    // res = ln(poly), poly[0] should be 1
+    void log (int poly[], int res[], int n) {
+        assert(poly[0] == 1);
+        inv (poly, tmp2, n);
+        for (int i = 0; i < n - 1; ++i) {
+            res[i] = (ll) poly[i + 1] * (i + 1) % mod;
+        }
+        fft::conv (res, tmp2, n - 1, n, mod, res);
+        for (int i = n - 1; i >= 1; --i) {
+            res[i] = (ll) res[i - 1] * iv[i] % mod;
+        }
+        res[0] = 0;
+    }
+    
+    // res = exp(poly), poly[0] should be 0
+    void exp (int poly[], int res[], int n) {
+        assert(poly[0] == 0);
+        while (n & n - 1)n += lowbit(n);
+        if (n == 1) {
+            res[0] = 1;
+            return;
+        }
+        exp (poly, res, n>>1);
+        log (res, tmp3, n);
+        for (int i = 0; i < n; ++i) {
+            tmp3[i] = poly[i] - tmp3[i];
+            if (tmp3[i] < 0) tmp3[i] += mod;
+        }
+        if (++tmp3[0] == mod) tmp3[0] = 0;
+        fft::conv (tmp3, res, n, n, mod, res);
+        memset (res + n, 0, sizeof (*res) * n);
+    }
+    
+    // res = sqrt(poly), poly[0] should be 1
+    void sqrt (int poly[], int res[], int n) {
+        if (n == 1) {
+            res[0] = 1;
+            return;
+        }
+        sqrt (poly, res, n>>1);
+        inv (res, tmp2, n);
+        int s = n<<1;
+        memcpy (tmp1, poly, sizeof (*poly) * n);
+        memset (tmp1 + n, 0, sizeof (*tmp1) * n);
+        memset (res + n, 0, sizeof (*res) * n);
+        // NTT::trans(tmp1, s, 1); NTT::trans(res, s, 1); NTT::trans(tmp2, s, 1);
+        for (int i = 0; i < s; ++i) {
+            res[i] = ((ll) res[i] * res[i] + tmp1[i]) % mod * iv[2] % mod * tmp2[i] % mod;
+        }
+        // NTT::trans(res, s, -1)
+        memset (res + n, 0, sizeof (*res) * n);
+    }
+}
+
+// polynomial arithmetic in O(n^2)
+// mod should be a prime greater than n
+namespace poly_brutal {
+    // f = f * g mod x^n
+    void conv (int n, ll f[], ll g[], ll mod) {
+        for (int i = n - 1; i >= 0; --i) {
+            ll tmp = 0;
+            for (int j = 0; j <= i; ++j) {
+                tmp += f[j] * g[i - j] % mod;
+            }
+            f[i] = tmp % mod;
+        }
+    }
+    
+    // f = g^{-1} mod x^n, g[0] != 0
+    void inv (int n, ll f[], ll g[], ll mod) {
+        assert(g[0] != 0);
+        ll inv0 = inverse (g[0], mod);
+        for (int i = 0; i < n; ++i) {
+            ll tmp = 0;
+            for (int j = 0; j < i; ++j) {
+                tmp += f[j] * g[i - j] % mod;
+            }
+            if (!i) f[i] = inv0;
+            else f[i] = (mod - tmp % mod) * inv0 % mod;
+        }
+    }
+    
+    // f = ln(g) mod x^n, g[0] = 1
+    void log (int n, ll f[], ll g[], ll mod) {
+        assert(g[0] == 1);
+        inv (n, f, g, mod);
+        for (int i = n - 1; i >= 0; --i) {
+            ll tmp = 0;
+            for (int j = 0; j <= i; ++j) {
+                tmp += f[i - j] * (j == n ? 0 : g[j + 1]) % mod * (j + 1) % mod;
+            }
+            f[i] = tmp % mod;
+        }
+        for (int i = n - 1; i >= 1; --i) {
+            f[i] = f[i - 1] * inverse (i, mod) % mod;
+        }
+        f[0] = 0;
+    }
+    
+    // f= exp(g) mod x^n, g[0] = 0
+    void exp (int n, ll f[], ll g[], ll mod) {
+        assert(g[0] == 0);
+        f[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            ll tmp = 0;
+            for (int j = 1; j <= i; ++j) {
+                tmp += j * g[j] % mod * f[i - j] % mod;
+            }
+            f[i] = tmp % mod * inverse (i, mod) % mod;
+        }
+    }
+}
