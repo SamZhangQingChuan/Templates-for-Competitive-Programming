5d6924e3acaa9d76abb057addcebd67097beed7e
samzhang <samzhangqingchuan@icloud.com>
2020-06-08 17:10:18 +1200

    Update Xor-Basis.cpp

--- 数学/Xor-Basis.cpp
+++ 数学/Xor-Basis.cpp
@@ -1,40 +1,38 @@
 struct Xor_Basis {
     ull base[64];
     
-    Xor_Basis() {
+    Xor_Basis () {
         mst(base, 0);
     }
     
-    void add(ull element) {
-        RREP(i, 63, 0)
-        {
-            if(element & twop(i)) {
-                if(base[i]) element ^= base[i];
+    bool add (ull element) {
+        RREP(i, 63, 0) {
+            if (element & twop (i)) {
+                if (base[i]) element ^= base[i];
                 else {
                     base[i] = element;
-                    break;
+                    return true;
                 }
             }
         }
+        return false;
     }
     
-    ull find_max(ull element) const {
-        RREP(i, 63, 0)
-        {
-            if(!(element & twop(i))) {
+    ull find_max (ull element) const {
+        RREP(i, 63, 0) {
+            if (!(element & twop (i))) {
                 element ^= base[i];
             }
         }
         return element;
     }
     
-    ull find_min(ull element) const {
-        RREP(i, 63, 0)
-        {
-            if((element & twop(i))) {
+    ull find_min (ull element) const {
+        RREP(i, 63, 0) {
+            if ((element & twop (i))) {
                 element ^= base[i];
             }
         }
         return element;
     }
-} basis;
+};
