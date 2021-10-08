3658bfb512f60e0f4b755ee3cc204ec9849d0e7a
samzhang <samzhangqingchuan@icloud.com>
2021-10-08 22:45:05 +0800

    amend Template.cpp

--- 头文件及测试/Template.cpp
+++ 头文件及测试/Template.cpp
@@ -1,15 +1,3 @@
-#pragma comment(linker, "/stack:200000000")
-#pragma GCC optimize("Ofast")
-//#pragma GCC optimize(3)
-//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-//#pragma GCC target("sse3","sse2","sse")
-//#pragma GCC target("avx","sse4","sse4.1","sse4.2","ssse3")
-//#pragma GCC target("f16c")
-//#pragma GCC optimize("inline","fast-math","unroll-loops","no-stack-protector")
-//#pragma GCC diagnostic error "-fwhole-program"
-//#pragma GCC diagnostic error "-fcse-skip-blocks"
-//#pragma GCC diagnostic error "-funsafe-loop-optimizations"
-//#pragma GCC diagnostic error "-std=c++14"
 #include "bits/stdc++.h"
 #include "ext/pb_ds/tree_policy.hpp"
 #include "ext/pb_ds/assoc_container.hpp"
@@ -20,8 +8,8 @@
 #define UB upper_bound
 #define fr(x) freopen(x,"r",stdin)
 #define fw(x) freopen(x,"w",stdout)
-#define REP(x, l, u) for(ll x = l;x<u;x++)
-#define RREP(x, l, u) for(ll x = l;x>=u;x--)
+#define REP(i, from, to) for(ll i = from;i<to;x++)
+#define RREP(i, from, to) for(ll i = from;i>=to;x--)
 #define complete_unique(a) a.erase(unique(begin(a),end(a)),end(a))
 #define mst(x, a) memset(x,a,sizeof(x))
 #define all(a) begin(a),end(a)
@@ -50,34 +38,34 @@ typedef vector<int> VI;
 const ll mod = 1e9 + 7;
 
 
-string to_string (string s) { return '"' + s + '"'; }
+string to_string(string s) { return '"' + s + '"'; }
 
-string to_string (const char *s) { return to_string ((string) s); }
+string to_string(const char *s) { return to_string((string) s); }
 
-string to_string (bool b) { return (b ? "true" : "false"); }
+string to_string(bool b) { return (b ? "true" : "false"); }
 
 template<typename A, typename B>
-string to_string (pair<A, B> p) { return "(" + to_string (p.first) + ", " + to_string (p.second) + ")"; }
+string to_string(pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
 
 template<typename A>
-string to_string (A v) {
+string to_string(A v) {
     bool first = true;
     string res = "{";
-    for (const auto &x : v) {
-        if (!first) { res += ", "; }
+    for(const auto &x: v) {
+        if(!first) { res += ", "; }
         first = false;
-        res += to_string (x);
+        res += to_string(x);
     }
     res += "}";
     return res;
 }
 
-void debug_out () { cerr<<endl; }
+void debug_out() { cerr << endl; }
 
 template<typename Head, typename... Tail>
-void debug_out (Head H, Tail... T) {
-    cerr<<" "<<to_string (H);
-    debug_out (T...);
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
 }
 
 #ifdef LOCAL
@@ -87,44 +75,35 @@ void debug_out (Head H, Tail... T) {
 #endif
 
 template<typename T, typename S>
-inline bool upmin (T &a, const S &b) { return a > b ? a = b, 1 : 0; }
+inline bool upmin(T &a, const S &b) { return a > b ? a = b, 1 : 0; }
 
 template<typename T, typename S>
-inline bool upmax (T &a, const S &b) { return a < b ? a = b, 1 : 0; }
+inline bool upmax(T &a, const S &b) { return a < b ? a = b, 1 : 0; }
 
 
-ull twop (ll x) { return 1ULL<<x; }
+ull twop(ll x) { return 1ULL << x; }
 
-ll MOD (ll a, ll m) {
+ll MOD(ll a, ll m) {
     a %= m;
-    if (a < 0)a += m;
+    if(a < 0)a += m;
     return a;
 }
 
-ll inverse (ll a, ll m) {
-    a = MOD (a, m);
-    if (a <= 1)return a;
-    return MOD ((1 - inverse (m, a) * m) / a, m);
+ll inverse(ll a, ll m) {
+    a = MOD(a, m);
+    if(a <= 1)return a;
+    return MOD((1 - inverse(m, a) * m) / a, m);
 }
 
 template<typename T>
-T sqr (T x) { return x * x; }
+T sqr(T x) { return x * x; }
 
-ll gcd (ll a, ll b) {
-    a = abs (a), b = abs (b);
-    while (b != 0) {
-        a %= b;
-        swap (a, b);
-    }
-    return a;
-}
-
-ll fast (ll a, ll b, ll mod) {
+ll fast(ll a, ll b, ll mod) {
     a %= mod;
-    if (b < 0)a = inverse (a, mod), b = -b;
+    if(b < 0)a = inverse(a, mod), b = -b;
     ll ans = 1;
-    while (b) {
-        if (b & 1)ans = ans * a % mod;
+    while(b) {
+        if(b & 1)ans = ans * a % mod;
         a = a * a % mod;
         b /= 2;
     }
@@ -133,27 +112,26 @@ ll fast (ll a, ll b, ll mod) {
 
 
 namespace SOLVE {
-    void main () {
+    void main() {
     
     }
 }
 
-
-signed main () {
+signed main() {
 #ifdef LOCAL
     fr("/Users/zhangqingchuan/Desktop/cp/cp/input.txt");
     fw("/Users/zhangqingchuan/Desktop/cp/cp/output.txt");
 #endif
-    ios::sync_with_stdio (false);
-    cin.tie (nullptr);
-    cout.tie (nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     
     
     int t = 1;
 //    cin >> t;
-    for (int i = 1; i <= t; i++) {
+    for(int i = 1; i <= t; i++) {
 //        cout<<"Case #"<<i<<": ";
-        SOLVE::main ();
+        SOLVE::main();
         
     }
 
