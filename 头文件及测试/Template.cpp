ded2773ff03264b7cc900de0f28397ed659b243c
Qingchuan Zhang <qzha536@aucklanduni.ac.nz>
2022-01-16 02:17:12 +0800

    minor fix

--- 头文件及测试/Template.cpp
+++ 头文件及测试/Template.cpp
@@ -121,8 +121,8 @@ namespace SOLVE {
 }
 
 #ifdef __APPLE__
-#define INPUT_FILE "/Users/qingczha/Desktop/cp/input.txt"
-#define OUTPUT_FILE "/Users/qingczha/Desktop/cp/output.txt"
+#define INPUT_FILE "/Users/qingczha/ClionProjects/Playground/input.txt"
+#define OUTPUT_FILE "/Users/qingczha/ClionProjects/Playground/output.txt"
 #else
 #define INPUT_FILE ""
 #define OUTPUT_FILE ""
