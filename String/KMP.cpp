aaf666ba6f54979df28d8b2abc6cdd84fc5f3bf6
qingczha <samzhangqingchuan@icloud.com>
2024-07-14 10:28:01 +0800

    beautify

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -1,6 +1,10 @@
-void calcNext(const string &s, int nxt[]) {
+#include <string>
+#include <vector>
+#include <algorithm>
+
+void calcNext(const std::string &s, int nxt[]) {
     int p = nxt[0] = -1;
-    REP(i, 1, sz(s)){
+    for (int i = 1; i < s.size(); i++) {
         while (p != -1 && s[p + 1] != s[i])p = nxt[p];
         p += s[p + 1] == s[i];
         nxt[i] = p;
@@ -8,13 +12,13 @@ void calcNext(const string &s, int nxt[]) {
 }
 
 
-vector<int> exkmp(const string &s) {
+std::vector<int> exkmp(const std::string &s) {
     int n = s.size();
-    vector<int> z(n);
+    std::vector<int> z(n);
     int x = 0, y = 0;
-    for(int i = 1; i < n; i++) {
-        z[i] = max(0, min(z[i - x], y - i + 1));
-        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+    for (int i = 1; i < n; i++) {
+        z[i] = std::max(0, std::min(z[i - x], y - i + 1));
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
             x = i;
             y = i + z[i];
             z[i]++;
